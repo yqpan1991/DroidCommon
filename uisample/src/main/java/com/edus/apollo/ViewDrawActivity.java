61a0a46d9c22991ede74fdbe3aed2d847ffb73d2
panyongqiang <yqpan1991@163.com>
2017-02-16 08:40:17 +0800

    添加宽高比固定的imageview

--- uisample/src/main/java/com/edus/apollo/ViewDrawActivity.java
+++ uisample/src/main/java/com/edus/apollo/ViewDrawActivity.java
@@ -1,14 +1,17 @@
 package com.edus.apollo;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.edus.apollo.common.ui.view.AspectRatioImageView;
+
 public class ViewDrawActivity extends AppCompatActivity implements View.OnClickListener{
     private LevelFrameLayout mLflContent;
     private TextView mTvContent;
+    private AspectRatioImageView mArivTest;
 
 
     @Override
@@ -19,6 +22,23 @@ public class ViewDrawActivity extends AppCompatActivity implements View.OnClickL
         mTvContent = (TextView) findViewById(R.id.tv_content);
         mTvContent.setText("hello world, what's your name");
         mLflContent.setLevel(2);
+        mArivTest = (AspectRatioImageView) findViewById(R.id.ariv_test);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mArivTest.setHeightWidthRatio(0.5625F);
+                mArivTest.setDominantMeasurement(AspectRatioImageView.MEASUREMENT_WIDTH);
+                mArivTest.setHeightWidthRatioEnabled(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mArivTest.setHeightWidthRatioEnabled(false);
+                        mArivTest.setDominantMeasurement(AspectRatioImageView.MEASUREMENT_HEIGHT);
+                    }
+                },2000);
+            }
+        },2000);
     }
 
     @Override
--- uisample/src/test/java/com/edus/apollo/ExampleUnitTest.java
+++ uisample/src/test/java/com/edus/apollo/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package com.edus.apollo;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,5 +16,21 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void setTest(){
+        SortedSet<String> sortedSet = new TreeSet<>();
+        Collections.addAll(sortedSet, "one two three four five six seven eight".split(" "));
+        Iterator<String> it = sortedSet.iterator();
+        String low = null;
+        String high = null;
+        for(int i =0; i<=6; i++){
+            if(i ==3)
+                low = it.next();
+            if(i ==6) high = it.next();
+            else it.next();
+        }
+        System.out.println("low:"+low+",high:"+high);
     }
 }
