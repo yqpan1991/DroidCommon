0de628045fd55e44cab16b664748b1b374f7068b
PandaPan <280853071@qq.com>
2017-12-17 15:21:30 +0800

    更新相关配置文件

--- uisample/src/main/java/com/edus/apollo/MainActivity.java
+++ uisample/src/main/java/com/edus/apollo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WindowManager windowManager = null;
+        getSupportFragmentManager().executePendingTransactions();
     }
 
     @Override
